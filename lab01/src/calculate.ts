ea84434c7a9260f8cc122872c59e2014cbfc0be5
sofya <sofya@MacBook-Air-sofya-2.local>
2025-09-15 12:57:07 +0700

    initial commit for 1 lab

--- lab01/src/calculate.ts
+++ lab01/src/calculate.ts
@@ -5,7 +5,23 @@ export const addMulSemantics: AddMulSemantics = grammar.createSemantics() as Add
 
 
 const addMulCalc = {
-/// write the action rules here
+    // Exp(e) {
+    //     return e.calculate();
+    // }, 
+    AddExp(x, _plus, y) {
+        return x.calculate() + (y.numChildren > 0 
+            ? y.children[0].calculate()
+            : 0);
+    }, 
+    MulExp_times(x, _times, y) {
+        return x.calculate() * y.calculate();
+    },
+    PriExp_paren(_open, e, _close) {
+        return e.calculate();
+    },
+    number(_digits) {
+        return parseInt(this.sourceString, 10);
+    },
 } satisfies AddMulActionDict<number>
 
 addMulSemantics.addOperation<Number>("calculate()", addMulCalc);
--- lab01/src/index.ts
+++ lab01/src/index.ts
@@ -6,16 +6,21 @@ export function evaluate(content: string): number
 {
     return calculate(parse(content));
 }
+
 export class SyntaxError extends Error
 {
 }
 
 function parse(content: string): MatchResult
 {
-    throw "Not implemented";
+    const match = grammar.match(content);
+    if (match.failed()) {
+        throw new SyntaxError(match.message);
+    }
+    return match;
 }
 
 function calculate(expression: MatchResult):number
 {
-    throw "Not implemented"
+    return addMulSemantics(expression).calculate();
 }
